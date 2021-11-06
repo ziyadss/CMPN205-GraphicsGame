62d2407ee2da9125cc41a804495cea8f583f300c
nadaabdelgayed <nada.ahmed15@stemmaadi.edu.eg>
2021-11-06 15:54:31 +0200

    Implemented shader.hpp and line.frag

--- assets/shaders/line.frag
+++ assets/shaders/line.frag
@@ -6,12 +6,14 @@ out vec4 frag_color;
 
 uniform vec4 inside_color = vec4(1.0, 0.0, 0.0, 1.0);
 uniform vec4 outside_color = vec4(0.0, 0.0, 0.0, 1.0);
+uniform float intercept = 0;
+uniform float slope = 1.0;
 
 void main(){
     //TODO: Write code that will draw the square
-    if(gl_FragCoord.x <= 256){
+    float y_coordinate = slope*gl_FragCoord.x+intercept;
+    if(gl_FragCoord.y <= y_coordinate)
         frag_color = inside_color;
-    } else {
+    else
         frag_color = outside_color;
-    }
 }
--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -7,9 +7,11 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-namespace our {
+namespace our
+{
 
-    class ShaderProgram {
+    class ShaderProgram
+    {
 
     private:
         //Shader Program Handle
@@ -19,40 +21,57 @@ namespace our {
         void create();
         void destroy();
 
-        ShaderProgram(){ program = 0; }
-        ~ShaderProgram(){ destroy(); }
+        ShaderProgram() { program = 0; }
+        ~ShaderProgram() { destroy(); }
 
         bool attach(const std::string &filename, GLenum type) const;
 
         bool link() const;
 
-        void use() { 
+        void use()
+        {
             //TODO: call opengl to use the program identified by this->program
+            glUseProgram(this->program);
         }
 
-        GLuint getUniformLocation(const std::string &name) {
+        GLuint getUniformLocation(const std::string &name)
+        {
             //TODO: call opengl to get the uniform location for the uniform defined by name from this->program
+            GLuint nameLoc = glGetUniformLocation(this->program, name.c_str());
+            return nameLoc;
         }
 
-        void set(const std::string &uniform, GLfloat value) {
+        void set(const std::string &uniform, GLfloat value)
+        {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform1f(getUniformLocation(uniform), value);
         }
 
-        void set(const std::string &uniform, glm::vec2 value) {
+        void set(const std::string &uniform, glm::vec2 value)
+        {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform2fv(getUniformLocation(uniform), 1, glm::value_ptr(value));
         }
 
-        void set(const std::string &uniform, glm::vec3 value) {
+        void set(const std::string &uniform, glm::vec3 value)
+        {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform3fv(getUniformLocation(uniform), 1, glm::value_ptr(value));
         }
 
-        void set(const std::string &uniform, glm::vec4 value) {
+        void set(const std::string &uniform, glm::vec4 value)
+        {
             //TODO: call opengl to set the value to the uniform defined by name
+            glUniform4fv(getUniformLocation(uniform), 1, glm::value_ptr(value));
         }
-
 
         //TODO: Delete the copy constructor and assignment operator
+        ShaderProgram(const ShaderProgram &) = delete;
+        ShaderProgram &operator=(const ShaderProgram &) = delete;
         //Question: Why do we do this? Hint: Look at the deconstructor
+        /*Because if we create two program pointers and assign to them the same value then ended the program by deleting one of them, 
+        The other one will still point to the same place in memory and can be used to access data that is no longer there.
+        We solve this problem by deleting the copy constructor and assignment operator and therefore not allowing this situation to happen*/
     };
 
 }
