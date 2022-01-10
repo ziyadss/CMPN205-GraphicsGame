f3d49191d5edd584bb879a58741d485c168ad50f
Ziyad <ziyad.ss@hotmail.com>
2022-01-10 23:27:06 +0200

    Update menu-state.hpp

--- Phase 2 + 3/source/states/menu-state.hpp
+++ Phase 2 + 3/source/states/menu-state.hpp
@@ -8,16 +8,15 @@ class MenuState : public our::State
 {
     our::World world;
     our::ForwardRenderer renderer;
-    our::FreeCameraControllerSystem cameraController;
-    our::FreePlayerControllerSystem playerController;
+    our::PlayerControllerSystem playerController;
 
     void onInitialize() override
     {
         auto &config = getApp()->getConfig()["scene"];
-        
+
         if (config.contains("assets"))
             our::deserializeAllAssets(config["assets"]);
-        
+
         if (config.contains("world"))
             world.deserialize(config["world"]);
     }
@@ -27,12 +26,12 @@ class MenuState : public our::State
         playerController.update(&world, (float)deltaTime);
 
         auto size = getApp()->getFrameBufferSize();
-        renderer.render(&world, glm::ivec2(0,0), size);
+        renderer.render(&world, glm::ivec2(0, 0), size);
     }
 
     void onDestroy() override
     {
-        cameraController.exit();
+        playerController.exit();
 
         our::clearAllAssets();
     }
