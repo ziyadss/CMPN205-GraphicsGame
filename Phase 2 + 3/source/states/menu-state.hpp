404cb7988b708ce6cad155ada52cd50111a7a78d
Huchra <65508021+Huchra@users.noreply.github.com>
2022-01-10 23:15:32 +0200

    idk man

--- Phase 2 + 3/source/states/menu-state.hpp
+++ Phase 2 + 3/source/states/menu-state.hpp
@@ -6,16 +6,34 @@
 
 class MenuState : public our::State
 {
+    our::World world;
+    our::ForwardRenderer renderer;
+    our::FreeCameraControllerSystem cameraController;
+    our::FreePlayerControllerSystem playerController;
 
     void onInitialize() override
     {
+        auto &config = getApp()->getConfig()["scene"];
+        
+        if (config.contains("assets"))
+            our::deserializeAllAssets(config["assets"]);
+        
+        if (config.contains("world"))
+            world.deserialize(config["world"]);
     }
 
     void onDraw(double deltaTime) override
     {
+        playerController.update(&world, (float)deltaTime);
+
+        auto size = getApp()->getFrameBufferSize();
+        renderer.render(&world, glm::ivec2(0,0), size);
     }
 
     void onDestroy() override
     {
+        cameraController.exit();
+
+        our::clearAllAssets();
     }
 };
